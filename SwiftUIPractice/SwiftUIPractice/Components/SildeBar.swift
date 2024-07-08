c67cbce73a14551769b859d9b0d3e374cc26ef79
David-11209 <limonchik087@gmail.com>
2024-07-08 16:51:47 +0300

    Update SildeBar.swift

--- SwiftUIPractice/SwiftUIPractice/Components/SildeBar.swift
+++ SwiftUIPractice/SwiftUIPractice/Components/SildeBar.swift
@@ -10,128 +10,101 @@ struct SlideBar: View {
     @State private var isBarExpanded = false
 
     var body: some View {
-        GeometryReader { geometry in
-            ZStack(alignment: .leading) {
-                Color.white
-                    .edgesIgnoringSafeArea(.all)
+        HStack(spacing: 0) {
+            VStack(spacing: 10) {
+                Button(action: {
 
-                HStack(spacing: 0) {
-                    VStack {
-                        Spacer()
-
-                        VStack(spacing: 10) {
-                            Button(action: {
-
-                            }) {
-                                Image(systemName: "book.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 50, height: 50)
-                            }
-                            Text("Русский язык")
-                                .font(.system(size: 12))
-                                .multilineTextAlignment(.center)
-
-                            Button(action: {
-
-                            }) {
-                                Image(systemName: "book.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 50, height: 50)
-                            }
-                            Text("Русский язык")
-                                .font(.system(size: 12))
-                                .multilineTextAlignment(.center)
-
-                            Button(action: {
-
-                            }) {
-                                Image(systemName: "book.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 50, height: 50)
-                            }
-                            Text("Русский язык")
-                                .font(.system(size: 12))
-                                .multilineTextAlignment(.center)
-
-                            Button(action: {
-
-                            }) {
-                                Image(systemName: "book.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 50, height: 50)
-                            }
-                            Text("Русский язык")
-                                .font(.system(size: 12))
-                                .multilineTextAlignment(.center)
-
-                            Button(action: {
-
-                            }) {
-                                Image(systemName: "book.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 50, height: 50)
-                            }
-                            Text("Русский язык")
-                                .font(.system(size: 12))
-                                .multilineTextAlignment(.center)
-                        }
-                        .padding()
-                        .frame(width: isBarExpanded ? 90 : 0, height: geometry.size.height)
-                        .clipped()
-                        .background(Color.gray)
-                        .animation(.default, value: isBarExpanded)
-
-                        Spacer()
-                    }
-
-                    RoundedRectangle(cornerRadius: 10, style: .continuous)
-                        .fill(Color.gray)
-                        .ignoresSafeArea(.all)
-                        .frame(width: 22, height: isBarExpanded ?  geometry.size.height:  geometry.size.height * 0.15)
-                        .offset(x: -6)
-                        .gesture(DragGesture()
-                            .onChanged { value in
-                                if value.translation.width > 50 {
-                                    withAnimation {
-                                        isBarExpanded = true
-                                    }
-                                }
-                                if value.translation.width < -50 {
-                                    withAnimation {
-                                        isBarExpanded = false
-                                    }
-                                }
-                            }
-                        )
-                        .overlay(
-                            Image(systemName: "chevron.right")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 8, height: 8)
-                                .rotationEffect(.degrees(isBarExpanded ? 180 : 0))
-                                .animation(.default, value: isBarExpanded)
-                                .foregroundColor(.white)
-                        )
+                }) {
+                    Image(systemName: "book.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
                 }
-                .animation(.default, value: isBarExpanded)
+                Text("Русский язык")
+                    .font(.system(size: 12))
+                    .multilineTextAlignment(.center)
 
-                if isBarExpanded {
-                    Color.clear
-                        .contentShape(Rectangle())
-                        .edgesIgnoringSafeArea(.all)
-                        .onTapGesture {
-                            withAnimation {
-                                isBarExpanded = false
-                            }
-                        }
+                Button(action: {
+
+                }) {
+                    Image(systemName: "book.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                }
+                Text("Русский язык")
+                    .font(.system(size: 12))
+                    .multilineTextAlignment(.center)
+
+                Button(action: {
+
+                }) {
+                    Image(systemName: "book.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                }
+                Text("Русский язык")
+                    .font(.system(size: 12))
+                    .multilineTextAlignment(.center)
+
+                Button(action: {
+
+                }) {
+                    Image(systemName: "book.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                }
+                Text("Русский язык")
+                    .font(.system(size: 12))
+                    .multilineTextAlignment(.center)
+
+                Button(action: {
+
+                }) {
+                    Image(systemName: "book.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                }
+                Text("Русский язык")
+                    .font(.system(size: 12))
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+            .frame(maxWidth: isBarExpanded ? 90 : 0, maxHeight: .infinity)
+            .clipped()
+            .background(Color.gray)
+            .animation(.default.speed(1), value: isBarExpanded)
+
+            RoundedRectangle(cornerRadius: isBarExpanded ? 0 : 10, style: .continuous)
+                .fill(Color.gray)
+                .frame(maxWidth: 22, maxHeight: isBarExpanded ?  .infinity :  120)
+                .offset(x: -6)
+                .overlay(alignment: .leading) {
+                    Image(systemName: "chevron.right")
+                        .resizable()
+                        .frame(width: 8, height: 8)
+                        .rotationEffect(.degrees(isBarExpanded ? 180 : 0))
+                        .animation(.default.speed(2), value: isBarExpanded)
+                        .foregroundColor(.white)
+                }
+        }
+        .gesture(DragGesture()
+            .onEnded { value in
+                if value.translation.width > 0 {
+                    isBarExpanded = true
+                } else {
+                    isBarExpanded = false
                 }
             }
+        )
+        .animation(.default, value: isBarExpanded)
+        .onTapGesture {
+            isBarExpanded = false
         }
+        .frame(width: .infinity, height: .infinity)
         .ignoresSafeArea(.all)
     }
 }
