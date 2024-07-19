a70d7632cdd6ace2b573d2c8cb388ad2f925db03
David-11209 <limonchik087@gmail.com>
2024-07-19 11:49:08 +0300

    update viewmodel  and preview

--- SwiftUIPractice/SwiftUIPractice/Modules/Canvas/CanvasView.swift
+++ SwiftUIPractice/SwiftUIPractice/Modules/Canvas/CanvasView.swift
@@ -20,7 +20,7 @@ struct CanvasView: View {
         ZStack {
             Color.white
             ZStack {
-                ForEach(viewModel.getCanvasData()) { shape in
+                ForEach(viewModel.elementsArray) { shape in
                     DraggableResizableView(rotation: Angle(degrees: shape.rotation ?? 0.0), shape: shape, isEditing: isEditing, image:  shape.imageId != nil ? viewModel.imagesDict[shape.imageId!] : nil)
                         .zIndex(shape.zIndex ?? 0.0)
                         .offset(dragOffset)
@@ -70,7 +70,99 @@ struct CanvasView: View {
 }
 
 #Preview {
-    var shapes: [CanvasElementModel]
+    let jsonData = """
+    {
+        "imageDictionary": {},
+        "shapes": [
+            {
+                "id": 1,
+                "type": "rectangle",
+                "x": 170,
+                "y": 100,
+                "width": 200,
+                "height": 100,
+                "color": "#FF5733",
+                "borderColor": "#C70039",
+                "zIndex": 0.5,
+                "cornerRadius": 20,
+                "borderWidth": 5,
+                "rotation":90
+            },
+            {
+                "id": 2,
+                "type": "image",
+                "x": 200,
+                "y": 600,
+                "width": 150,
+                "height": 150,
+                "imageId": "1",
+                "zIndex": 0.7,
+                "cornerRadius": 40
+            },
+            {
+                "id": 3,
+                "type": "arrow",
+                "x": 200,
+                "y": 200,
+                "width": 250,
+                "height": 150,
+                "borderColor": "#C70039",
+                "zIndex": 0.2,
+                "rotation":110
+            },
+            {
+                "id": 4,
+                "type": "rectangle",
+                "x": 170,
+                "y": 100,
+                "width": 200,
+                "height": 100,
+                "color": "#FF5733",
+                "borderColor": "#C70039",
+                "zIndex": 0.5,
+                "textVerticalAlignment": "top",
+                "text": [
+                    {
+                        "alignment": "left",
+                        "text": [
+                            {
+                                "text": "Hello",
+                                "type": "bold",
+                                "fontSize": 24,
+                                "fontColor": "#333333"
+                            },
+                            {
+                                "text": " This is a test text.",
+                                "fontSize": 18,
+                                "fontColor": "#333333"
+                            }
+                        ]
+                    },
+                    {
+                        "alignment": "right",
+                        "text": [
+                            {
+                                "text": "Hello",
+                                "type": "bold",
+                                "fontSize": 24,
+                                "fontColor": "#333333"
+                            },
+                            {
+                                "text": " This is a test text.",
+                                "fontSize": 18,
+                                "fontColor": "#333333"
+                            }
+                        ]
+                    }
+
+                ],
+                "borderWidth": 5
+            }
+        ]
+    }
+    """.data(using: .utf8) ?? nil
+
     var viewModel: CanvasViewModel = CanvasViewModel()
+    viewModel.getCanvasData(jsonData: jsonData ?? Data())
     return CanvasView()
 }
--- SwiftUIPractice/SwiftUIPractice/Modules/Canvas/CanvasViewModel.swift
+++ SwiftUIPractice/SwiftUIPractice/Modules/Canvas/CanvasViewModel.swift
@@ -9,99 +9,8 @@ import SwiftUI
 
 class CanvasViewModel: ObservableObject {
     var imagesDict: [String: UIImage] = [:]
-    let jsonData = """
-    {
-        "imageDictionary": {},
-        "shapes": [
-            {
-                "id": 1,
-                "type": "rectangle",
-                "x": 170,
-                "y": 100,
-                "width": 200,
-                "height": 100,
-                "color": "#FF5733",
-                "borderColor": "#C70039",
-                "zIndex": 0.5,
-                "cornerRadius": 20,
-                "borderWidth": 5,
-                "rotation":90
-            },
-            {
-                "id": 2,
-                "type": "image",
-                "x": 200,
-                "y": 600,
-                "width": 150,
-                "height": 150,
-                "imageId": "1",
-                "zIndex": 0.7,
-                "cornerRadius": 40
-            },
-            {
-                "id": 3,
-                "type": "arrow",
-                "x": 200,
-                "y": 200,
-                "width": 250,
-                "height": 150,
-                "borderColor": "#C70039",
-                "zIndex": 0.2,
-                "rotation":110
-            },
-            {
-                "id": 4,
-                "type": "rectangle",
-                "x": 170,
-                "y": 100,
-                "width": 200,
-                "height": 100,
-                "color": "#FF5733",
-                "borderColor": "#C70039",
-                "zIndex": 0.5,
-                "textVerticalAlignment": "top",
-                "text": [
-                    {
-                        "alignment": "left",
-                        "text": [
-                            {
-                                "text": "Hello",
-                                "type": "bold",
-                                "fontSize": 24,
-                                "fontColor": "#333333"
-                            },
-                            {
-                                "text": " This is a test text.",
-                                "fontSize": 18,
-                                "fontColor": "#333333"
-                            }
-                        ]
-                    },
-                    {
-                        "alignment": "right",
-                        "text": [
-                            {
-                                "text": "Hello",
-                                "type": "bold",
-                                "fontSize": 24,
-                                "fontColor": "#333333"
-                            },
-                            {
-                                "text": " This is a test text.",
-                                "fontSize": 18,
-                                "fontColor": "#333333"
-                            }
-                        ]
-                    }
-
-                ],
-                "borderWidth": 5
-            }
-        ]
-    }
-    """.data(using: .utf8)!
-
-    func getCanvasData() -> [CanvasElementModel] {
+    var elementsArray: [CanvasElementModel] = []
+    func getCanvasData(jsonData: Data) {
         var shapesArray: [CanvasElementModel] = []
         var uiImageDictionary: [String: UIImage] = [:]
 
@@ -123,7 +32,7 @@ class CanvasViewModel: ObservableObject {
             print("Error decoding JSON: \(error)")
         }
         imagesDict = uiImageDictionary
-        return shapesArray
+        elementsArray = shapesArray
     }
 
     private func convertBase64ToUIImage(base64String: String) -> UIImage? {
@@ -131,3 +40,96 @@ class CanvasViewModel: ObservableObject {
         return UIImage(data: imageData)
     }
 }
+
+/// Пример json
+/*
+ {
+     "imageDictionary": {},
+     "shapes": [
+         {
+             "id": 1,
+             "type": "rectangle",
+             "x": 170,
+             "y": 100,
+             "width": 200,
+             "height": 100,
+             "color": "#FF5733",
+             "borderColor": "#C70039",
+             "zIndex": 0.5,
+             "cornerRadius": 20,
+             "borderWidth": 5,
+             "rotation":90
+         },
+         {
+             "id": 2,
+             "type": "image",
+             "x": 200,
+             "y": 600,
+             "width": 150,
+             "height": 150,
+             "imageId": "1",
+             "zIndex": 0.7,
+             "cornerRadius": 40
+         },
+         {
+             "id": 3,
+             "type": "arrow",
+             "x": 200,
+             "y": 200,
+             "width": 250,
+             "height": 150,
+             "borderColor": "#C70039",
+             "zIndex": 0.2,
+             "rotation":110
+         },
+         {
+             "id": 4,
+             "type": "rectangle",
+             "x": 170,
+             "y": 100,
+             "width": 200,
+             "height": 100,
+             "color": "#FF5733",
+             "borderColor": "#C70039",
+             "zIndex": 0.5,
+             "textVerticalAlignment": "top",
+             "text": [
+                 {
+                     "alignment": "left",
+                     "text": [
+                         {
+                             "text": "Hello",
+                             "type": "bold",
+                             "fontSize": 24,
+                             "fontColor": "#333333"
+                         },
+                         {
+                             "text": " This is a test text.",
+                             "fontSize": 18,
+                             "fontColor": "#333333"
+                         }
+                     ]
+                 },
+                 {
+                     "alignment": "right",
+                     "text": [
+                         {
+                             "text": "Hello",
+                             "type": "bold",
+                             "fontSize": 24,
+                             "fontColor": "#333333"
+                         },
+                         {
+                             "text": " This is a test text.",
+                             "fontSize": 18,
+                             "fontColor": "#333333"
+                         }
+                     ]
+                 }
+
+             ],
+             "borderWidth": 5
+         }
+     ]
+ }
+ */
